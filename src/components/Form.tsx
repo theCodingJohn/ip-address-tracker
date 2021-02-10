2cdda7d7d731da3f54b11eab210bfd8b6bbe2712
John Carlo Cunanan <codingjohn@outlook.com>
2021-02-10 18:45:21 +0800

    add a component where user's output data will be put

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,7 +9,22 @@ const Form = () => {
         <button className="submit-button"></button>
       </form>
       <div className="output-data">
-
+        <div className="output-data-wrapper ">
+          <h2>ip address</h2>
+          <p>192.188.145.212</p>
+        </div>
+        <div className="output-data-wrapper ">
+          <h2>location</h2>
+          <p>Brooklyn, NY <br/>10001</p>
+        </div>
+        <div className="output-data-wrapper ">
+          <h2>timezone</h2>
+          <p>UTC -05:00</p>
+        </div>
+        <div className="output-data-wrapper output-data-wrapper-last">
+          <h2>isp</h2>
+          <p>SpaceX Starlink</p>
+        </div>
       </div>
     </section>
   )
